00d5c082fdb90b91b2ac4dfe717b465fa6a779d2
Ahmed Hussain <masterahmedhussain@gmail.com>
2024-01-12 00:32:15 -0600

    initial commit

---
+++ src/main/java/com/ahmed/invoicemanagment/InvoicemanagmentApplication.java
@@ -0,0 +1,13 @@
+package com.ahmed.invoicemanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicemanagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicemanagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahmed/invoicemanagment/InvoicemanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmed.invoicemanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicemanagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
